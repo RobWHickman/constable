00444c897cde0d3eb646ac614d77450cd106476b
RobWHickman <robwhickman@gmail.com>
2025-09-19 22:54:30 +0100

    updated main.rs with new data types

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod data_types;
 use csv::Reader;
 use std::error::Error;
 use std::fs::File;
-use data_types::ElectionResult;
+use data_types::ConstituencyResult;
 
 const RESULTS_FILE: &str = "./data/results/HoC-GE2024-results-by-constituency.csv";
 
@@ -11,7 +11,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let election_results_file = File::open(RESULTS_FILE)?;
     let mut election_results_data = Reader::from_reader(election_results_file);
 
-    let mut election_results: Vec<ElectionResult> = Vec::new();
+    let mut election_results: Vec<ConstituencyResult> = Vec::new();
     let mut i: i64 = 0;
     for election_result in election_results_data.records() {
         let record = election_result?;
