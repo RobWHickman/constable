f0e49b41ea307f14b3c11437b6441c2e70d47642
RobWHickman <robwhickman@gmail.com>
2025-09-21 11:47:08 +0100

    add matching for parties enum

--- src/data_types.rs
+++ src/data_types.rs
@@ -1,8 +1,21 @@
+#[derive(Debug)]
 pub enum Country {
     England,
     Scotland,
     Wales,
     NorthernIreland,
+}
+
+impl From<&str> for Country{
+    fn from(country_string: &str) -> Self{
+        match country_string {
+            "England" => Country::England,
+            "Scotland" => Country::Scotland,
+            "Wales" => Country::Wales,
+            "Northern Ireland" => Country::NorthernIreland,
+            _ => panic!("Invalid Country {}", country_string)
+        }
+    }
 }
 
 pub enum Parties {
@@ -14,6 +27,21 @@ pub enum Parties {
     PlaidCymru,
     ScottishNational,
     Other
+}
+
+impl From<&str> for Parties{
+    fn from(party_string: &str) -> Self {
+        match party_string {
+            "Lab" => Parties::Labour,
+            "Con" => Parties::Conservatives,
+            "Lib" => Parties::LiberalDemocrats,
+            "Green" => Parties::Green,
+            "RUK" => Parties::Reform,
+            "PC" => Parties::PlaidCymru,
+            "SNP" => Parties::ScottishNational,
+            _ => Parties::Other
+        }
+    }
 }
 
 pub struct Constituency{
